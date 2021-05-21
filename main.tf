b9de09b236ad4e5782dc0a874e33d8a7ca56ed5f
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-05-21 15:44:43 +0200

    chore: terraform 0.15 update

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ data "aws_iam_policy_document" "default" {
 
 module "label" {
   enabled    = true
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.1"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.8.0"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
